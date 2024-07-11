e4e576393239d59b187749ee4fac76eb2a608467
Veera Gani Durga Uggu <veeraganidurga@gmail.com>
2024-07-11 13:16:15 +0530

    Implemented quiz bot Functionalaties

--- core/reply_factory.py
+++ core/reply_factory.py
@@ -28,25 +28,40 @@ def generate_bot_responses(message, session):
     return bot_responses
 
 
-def record_current_answer(answer, current_question_id, session):
-    '''
-    Validates and stores the answer for the current question to django session.
-    '''
-    return True, ""
+def record_current_answer(self,answer, current_question_id, session):
+    if not self.validate_answer(answer):
+        return "Invalid answer.Please try again."
+
+    if current_question_id not in self.user_responses:
+        self.user_responses[current_question_id] = []
+
+    self.user_responses[current_question_id].append(answer)
+    return True, " Answer Recorded"
 
 
-def get_next_question(current_question_id):
-    '''
-    Fetches the next question from the PYTHON_QUESTION_LIST based on the current_question_id.
-    '''
+def get_next_question(self,current_question_id):
+    current_index = self.current_question_index.get(current_question_id, 0)
 
-    return "dummy question", -1
+    if current_index >= len(self.questions):
+        return None
+
+    question = self.questions[current_index]
+    self.current_question_index[current_question_id] = current_index + 1
+
+    return question
 
 
-def generate_final_response(session):
-    '''
-    Creates a final result message including a score based on the answers
-    by the user for questions in the PYTHON_QUESTION_LIST.
-    '''
+def generate_final_response(self,session,current_question_id):
+    responses = self.user_responses.get(current_question_id, [])
+    score = self.calculate_score(responses)
+    tatal_questions = len(self.questions)    
 
-    return "dummy result"
+    return f"You scored {score} out of {tatal_questions}."
+
+
+def calculate_score(self, responses):
+    score = 0
+    for res in responses:
+        if self.is_correct(res):
+            score += 1
+    return score 
